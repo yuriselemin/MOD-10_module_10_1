50b759baafefcebc0daf7878103c8819a5ea3fba
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-08-05 23:03:30 +0300

    Update MOD-10_module_10_1.py

--- MOD-10_module_10_1.py
+++ MOD-10_module_10_1.py
@@ -11,18 +11,18 @@ def write_words(word_count, file_name):
             sleep(0.1)
     print(f'Завершилась запись в файл {file_name} ')
 
-start_time_functions = datetime.now()
 
+start_time_functions = datetime.now()
 write_words(10, 'variant1.txt')
 write_words(30, 'variant2.txt')
 write_words(200, 'variant3.txt')
 write_words(100, 'variant4.txt')
-
 end_time_functions = datetime.now()
 print(f'Разница работы функций: {end_time_functions - start_time_functions} секунд')
 
-start_time_threads = datetime.now()
 
+
+start_time_threads = datetime.now()
 threads = []
 for count, file_name in [(10, 'variant5.txt'), (30, 'variant6.txt'), (200, 'variant7.txt'), (100, 'variant8.txt')]:
     t = Thread(target=write_words, args=(count, file_name))
@@ -31,7 +31,6 @@ for count, file_name in [(10, 'variant5.txt'), (30, 'variant6.txt'), (200, 'vari
 
 for t in threads:
     t.join()
-
 end_time_threads = datetime.now()
 print(f'Разница работы потоков: {end_time_threads - start_time_threads} секунд')
 
